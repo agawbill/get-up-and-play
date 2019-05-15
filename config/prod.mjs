75af5f09cddd1d378cb4aa36b93410d7b5383f0d
anthony gawbill <gaw316@gmail.com>
2019-05-14 23:21:16 -0400

    this should finally work using es6

---
+++ config/prod.mjs
@@ -0,0 +1,10 @@
+export const prod = {
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookAppID: process.env.FACEBOOK_APP_ID,
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+  gmailPW: process.env.GMAILPW
+};
+
+// production mongo uri
