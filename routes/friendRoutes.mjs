4c8f746a3386f32c10356a5a74d52e3d28b100bc
anthony gawbill <gaw316@gmail.com>
2019-07-09 19:53:43 -0400

    hello

--- routes/friendRoutes.mjs
+++ routes/friendRoutes.mjs
@@ -101,9 +101,10 @@ export const friendRoutes = app => {
 
     console.log("filtered users after filter", filteredUsers);
     //
-    let index = filteredUsers.length - 1;
+    // let index = filteredUsers.length - 1;
 
-    while (index >= 0) {
+    for await (let value of filteredUsers[filteredUsers.length - 1]) {
+      value -= 1;
       if (filteredUsers[index] !== undefined) {
         console.log("the person, to make sure it's logged", person);
         let indexOfRequest = filteredRequests.indexOf(person.userId);
@@ -112,10 +113,21 @@ export const friendRoutes = app => {
           console.log("what's being removed", filteredUsers.splice(index, 1));
           filteredUsers.splice(index, 1);
         }
-      } else {
-        index -= 1;
       }
     }
+    // while (index >= 0) {
+    //   if (filteredUsers[index] !== undefined) {
+    //     console.log("the person, to make sure it's logged", person);
+    //     let indexOfRequest = filteredRequests.indexOf(person.userId);
+    //     console.log("the index", indexOfRequest);
+    //     if (indexOfRequest >= 0) {
+    //       console.log("what's being removed", filteredUsers.splice(index, 1));
+    //       filteredUsers.splice(index, 1);
+    //     }
+    //   } else {
+    //     index -= 1;
+    //   }
+    // }
 
     // filteredUsers.forEach((person, index) => {
     //   if (person !== undefined) {
